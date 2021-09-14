61a4201c91b6e096c5a539788a0b4c1ebb725c81
Sergii Kabashniuk <skabashniuk@redhat.com>
2021-09-14 11:42:10 +0300

    init ignore

---
+++ 02_init_and_unseale.sh
@@ -0,0 +1,11 @@
+while [ "$(kubectl get pods -l app.kubernetes.io/name=vault -n vault -o jsonpath='{.items[*].status.phase}')" != "Running" ]; do
+   sleep 5
+   echo "Waiting for vault to be ready."
+done
+
+kubectl exec -ti vault-0 -n vault -- sh -c 'vault operator init > /tmp/keys.txt'
+kubectl exec -ti vault-0 -n vault -- sh -c 'vault operator unseal $(grep -h '"'"'Unseal Key 1'"'"'  /tmp/keys.txt | awk '"'"'{print $NF}'"'"')'
+kubectl exec -ti vault-0 -n vault -- sh -c 'vault operator unseal $(grep -h '"'"'Unseal Key 2'"'"'  /tmp/keys.txt | awk '"'"'{print $NF}'"'"')'
+kubectl exec -ti vault-0 -n vault -- sh -c 'vault operator unseal $(grep -h '"'"'Unseal Key 3'"'"'  /tmp/keys.txt | awk '"'"'{print $NF}'"'"')'
+kubectl exec -ti vault-0 -n vault -- sh -c 'vault login $(grep -h '"'"'Initial Root Token'"'"' /tmp/keys.txt | awk '"'"'{print $NF}'"'"')'
+
